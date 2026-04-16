e8a68d89b158aa54c1b426dd94bde5891a94d0be
Mariana Gonzalez <mariana@mimiquate.com>
2026-04-16 18:34:14 -0300

    configure test environment paths, aliases and test deps

--- mix.exs
+++ mix.exs
@@ -7,9 +7,14 @@ defmodule TowerDB.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      aliases: aliases(),
       deps: deps()
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -22,7 +27,15 @@ defmodule TowerDB.MixProject do
   defp deps do
     [
       {:tower, "~> 0.7"},
-      {:ecto_sql, "~> 3.10"}
+      {:ecto_sql, "~> 3.10"},
+      {:postgrex, ">= 0.0.0", only: :test},
+      {:jason, "~> 1.4", only: :test}
+    ]
+  end
+
+  defp aliases do
+    [
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 end
