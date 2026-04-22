4b0ed53360d97c7a779d59a596f1259d1103579a
Mariana Gonzalez <mariana@mimiquate.com>
2026-04-22 14:31:43 -0300

    configure TestRepo in config.exs and remove test environment import

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,10 @@
 import Config
 
-if config_env() == :test do
-  import_config "test.exs"
-end
+config :tower_db, TowerDB.TestRepo,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  priv: "priv/test_repo",
+  url: System.get_env("POSTGRES_URL") || "postgres://localhost:5432/tower_db_test"
+
+config :tower_db,
+  repo: TowerDB.TestRepo,
+  ecto_repos: [TowerDB.TestRepo]
