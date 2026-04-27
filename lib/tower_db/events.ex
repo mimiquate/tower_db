dafa04793cfcf895df188e7e55d5333d68533b8e
Mariana Gonzalez <mariana@mimiquate.com>
2026-04-27 17:38:23 -0300

    add list_events functionn to context events

--- lib/tower_db/events.ex
+++ lib/tower_db/events.ex
@@ -1,6 +1,18 @@
 defmodule TowerDB.Events do
+  import Ecto.Query
+
   alias TowerDB.Event
   alias TowerDB.Repo
+
+  def list_events(opts \\ []) do
+    repo = Keyword.get(opts, :repo) || Repo.repo()
+    limit = Keyword.get(opts, :limit, 100)
+
+    Event
+    |> order_by(desc: :datetime)
+    |> limit(^limit)
+    |> repo.all()
+  end
 
   def create_event(attrs, opts \\ []) do
     repo = Keyword.get(opts, :repo) || Repo.repo()
