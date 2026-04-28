4e35298da05c6f0feabece5b28b872a78779734b
Mariana Gonzalez <mariana@mimiquate.com>
2026-04-28 17:06:13 -0300

    update events table name in migration file

--- priv/test_repo/migrations/20260416120000_create_events.exs
+++ priv/test_repo/migrations/20260416120000_create_events.exs
@@ -2,7 +2,7 @@ defmodule TowerDB.TestRepo.Migrations.CreateEvents do
   use Ecto.Migration
 
   def change do
-    create table(:events) do
+    create table(:tower_db_events) do
       add :datetime, :utc_datetime_usec, null: false
       add :level, :string, null: false
       add :reason, :binary, null: false
@@ -12,7 +12,7 @@ defmodule TowerDB.TestRepo.Migrations.CreateEvents do
       timestamps(type: :utc_datetime_usec)
     end
 
-    create index(:events, [:datetime])
-    create index(:events, [:level])
+    create index(:tower_db_events, [:datetime])
+    create index(:tower_db_events, [:level])
   end
 end
