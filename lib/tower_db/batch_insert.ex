7f0da58ba380d667cd8693178a241fe1ae267dd9
Mariana Gonzalez <mariana@mimiquate.com>
2026-05-04 10:45:41 -0300

    replace insert_all with Ecto.Multi to support custom types

--- lib/tower_db/batch_insert.ex
+++ lib/tower_db/batch_insert.ex
@@ -1,6 +1,7 @@
 defmodule TowerDB.BatchInsert do
   @moduledoc false
 
+  alias TowerDB.Event
   alias TowerDB.Repo
 
   require Logger
@@ -9,31 +10,22 @@ defmodule TowerDB.BatchInsert do
 
   def insert_all(events_attrs) when is_list(events_attrs) do
     repo = Repo.repo()
-    now = DateTime.utc_now()
 
-    rows =
-      Enum.map(events_attrs, fn attrs ->
-        %{
-          datetime: attrs.datetime,
-          level: to_string(attrs.level),
-          reason: serialize_term(attrs.reason),
-          stacktrace: serialize_term(attrs[:stacktrace]),
-          metadata: serialize_term(attrs[:metadata] || %{}),
-          inserted_at: now,
-          updated_at: now
-        }
+    multi =
+      events_attrs
+      |> Enum.with_index()
+      |> Enum.reduce(Ecto.Multi.new(), fn {attrs, index}, multi ->
+        changeset = Event.changeset(%Event{}, attrs)
+        Ecto.Multi.insert(multi, {:event, index}, changeset)
       end)
 
-    try do
-      {count, _} = repo.insert_all("tower_db_events", rows)
-      {:ok, count}
-    rescue
-      e ->
-        Logger.error("[TowerDB] Batch insert error: #{Exception.message(e)}")
-        {:error, e}
+    case repo.transaction(multi) do
+      {:ok, results} ->
+        {:ok, map_size(results)}
+
+      {:error, _name, changeset, _changes} ->
+        Logger.error("[TowerDB] Batch insert error: #{inspect(changeset.errors)}")
+        {:error, changeset}
     end
   end
-
-  defp serialize_term(nil), do: nil
-  defp serialize_term(term), do: :erlang.term_to_binary(term)
 end
