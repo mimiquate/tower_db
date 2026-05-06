fc606318912e89d95d5ae3737c9182c3d1f0d3bd
Mariana Gonzalez <mariana@mimiquate.com>
2026-05-06 13:28:06 -0300

    move migration file and update priv in config file

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ import Config
 
 config :tower_db, TowerDB.TestRepo,
   pool: Ecto.Adapters.SQL.Sandbox,
-  priv: "priv/test_repo",
+  priv: "test/support/test_repo",
   url: System.get_env("POSTGRES_URL") || "postgres://localhost:5432/tower_db_test",
   log: false
 
