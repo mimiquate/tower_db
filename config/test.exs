2cf285fd27ed9de624df90ba24ba6892baf04672
Mariana Gonzalez <mariana@mimiquate.com>
2026-06-22 17:12:06 -0300

    add test config

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,14 @@
 import Config
 
+config :logger, level: :none
+
+config :tower_db, :buffer,
+  batch_size: 5,
+  flush_interval: 100,
+  backpressure_retry: 50
+
 config :tower_db, :circuit_breaker,
   failure_threshold: 3,
-  recovery_timeout: 50,
+  recovery_timeout: 500,
   queue_retry_interval: 10,
   max_queue_size: 10
