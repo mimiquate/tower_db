7fc8251402f4405c1fedf3af0c2ab519cdad22cc
Mariana Gonzalez <mariana@mimiquate.com>
2026-06-25 13:57:30 -0300

    add pagination support for events

--- lib/tower_db/events.ex
+++ lib/tower_db/events.ex
@@ -4,12 +4,31 @@ defmodule TowerDB.Events do
   alias TowerDB.Event
   alias TowerDB.Repo
 
+  @default_limit 20
+
   def list_events(opts \\ []) do
     repo = Keyword.get(opts, :repo) || Repo.repo()
+    limit = Keyword.get(opts, :limit, @default_limit)
+    offset = Keyword.get(opts, :offset, 0)
 
     Event
     |> order_by(desc: :datetime)
+    |> limit(^limit)
+    |> offset(^offset)
     |> repo.all()
+  end
+
+  def count_events(opts \\ []) do
+    repo = Keyword.get(opts, :repo) || Repo.repo()
+
+    Event
+    |> repo.aggregate(:count)
+  end
+
+  def get_event(id, opts \\ []) do
+    repo = Keyword.get(opts, :repo) || Repo.repo()
+
+    repo.get(Event, id)
   end
 
   def create_event(attrs, opts \\ []) do
