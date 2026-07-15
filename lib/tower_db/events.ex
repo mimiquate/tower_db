b517f007d431a93756594227894754d11dfdf59e
Mariana Gonzalez <mariana@mimiquate.com>
2026-07-15 15:15:14 -0300

    remove cache logic

--- lib/tower_db/events.ex
+++ lib/tower_db/events.ex
@@ -4,8 +4,6 @@ defmodule TowerDB.Events do
   alias TowerDB.Event
   alias TowerDB.Repo
 
-  @cache_table :tower_db_events_cache
-
   @default_limit 20
 
   def list_events(opts \\ []) do
@@ -13,25 +11,11 @@ defmodule TowerDB.Events do
     limit = Keyword.get(opts, :limit, @default_limit)
     offset = Keyword.get(opts, :offset, 0)
 
-    try do
-      events =
-        Event
-        |> order_by(desc: :datetime)
-        |> limit(^limit)
-        |> offset(^offset)
-        |> repo.all()
-
-      # Cache successful result
-      cache_events(events)
-      events
-    rescue
-      _e ->
-        # Database unavailable, return cached events
-        get_cached_events()
-    catch
-      :exit, _reason ->
-        get_cached_events()
-    end
+    Event
+    |> order_by(desc: :datetime)
+    |> limit(^limit)
+    |> offset(^offset)
+    |> repo.all()
   end
 
   def count_events(opts \\ []) do
@@ -50,49 +34,9 @@ defmodule TowerDB.Events do
   def create_event(attrs, opts \\ []) do
     repo = Keyword.get(opts, :repo) || Repo.repo()
 
-    case %Event{}
-         |> Event.changeset(attrs)
-         |> repo.insert() do
-      {:ok, event} = result ->
-        add_event_to_cache(event)
-        result
-
-      error ->
-        error
-    end
-  end
-
-  # Cache functions
-
-  defp cache_events(events) do
-    ensure_cache_table()
-    :ets.insert(@cache_table, {:events, events})
-  end
-
-  defp add_event_to_cache(event) do
-    ensure_cache_table()
-    cached = get_cached_events()
-    # Prepend new event (list is ordered by datetime desc)
-    :ets.insert(@cache_table, {:events, [event | cached]})
-  end
-
-  defp get_cached_events do
-    ensure_cache_table()
-
-    case :ets.lookup(@cache_table, :events) do
-      [{:events, events}] -> events
-      [] -> []
-    end
-  end
-
-  defp ensure_cache_table do
-    case :ets.info(@cache_table) do
-      :undefined ->
-        :ets.new(@cache_table, [:set, :named_table, :public, read_concurrency: true])
-
-      _ ->
-        :ok
-    end
+    %Event{}
+    |> Event.changeset(attrs)
+    |> repo.insert()
   end
 
   def delete_event(%Event{} = event, opts \\ []) do
