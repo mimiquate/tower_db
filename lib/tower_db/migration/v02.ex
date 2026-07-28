feff9337efd5bda7247f13e62c98cd8ec5046d89
Mariana Gonzalez <mariana@mimiquate.com>
2026-07-28 17:08:36 -0300

    add and remove new fields if exists or not

--- lib/tower_db/migration/v02.ex
+++ lib/tower_db/migration/v02.ex
@@ -5,21 +5,21 @@ defmodule TowerDB.Migration.V02 do
 
   def up do
     alter table(:tower_db_events) do
-      add :similarity_id, :bigint
-      add :kind, :string
-      add :log_event, :binary
-      add :plug_conn, :binary
-      add :by, :string
+      add_if_not_exists :similarity_id, :bigint
+      add_if_not_exists :kind, :string
+      add_if_not_exists :log_event, :binary
+      add_if_not_exists :plug_conn, :binary
+      add_if_not_exists :by, :string
     end
   end
 
   def down do
     alter table(:tower_db_events) do
-      remove :by
-      remove :plug_conn
-      remove :log_event
-      remove :kind
-      remove :similarity_id
+      remove_if_exists :by, :string
+      remove_if_exists :plug_conn, :binary
+      remove_if_exists :log_event, :binary
+      remove_if_exists :kind, :string
+      remove_if_exists :similarity_id, :bigint
     end
   end
 end
