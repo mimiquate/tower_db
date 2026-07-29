6079f7ea4d24f3ea3072efc6c94849b703ef4b8a
Agustin Lopez <aln71198@gmail.com>
2026-07-29 15:35:05 -0300

    style: run mix format

--- lib/tower_db/schemas/event.ex
+++ lib/tower_db/schemas/event.ex
@@ -4,23 +4,38 @@ defmodule TowerDB.Event do
   import Ecto.Changeset
 
   schema "tower_db_events" do
-    field :similarity_id, :integer
-    field :datetime, :utc_datetime_usec
-    field :level, Ecto.Enum, values: [:debug, :info, :emergency, :alert, :critical, :error, :warning, :notice]
-    field :kind, Ecto.Enum, values: [:error, :exit, :throw, :message]
-    field :reason, TowerDB.Types.Term
-    field :stacktrace, TowerDB.Types.Term
-    field :log_event, TowerDB.Types.Term
-    field :plug_conn, TowerDB.Types.Term
-    field :metadata, TowerDB.Types.Term
-    field :by, :string
+    field(:similarity_id, :integer)
+    field(:datetime, :utc_datetime_usec)
+
+    field(:level, Ecto.Enum,
+      values: [:debug, :info, :emergency, :alert, :critical, :error, :warning, :notice]
+    )
+
+    field(:kind, Ecto.Enum, values: [:error, :exit, :throw, :message])
+    field(:reason, TowerDB.Types.Term)
+    field(:stacktrace, TowerDB.Types.Term)
+    field(:log_event, TowerDB.Types.Term)
+    field(:plug_conn, TowerDB.Types.Term)
+    field(:metadata, TowerDB.Types.Term)
+    field(:by, :string)
 
     timestamps(type: :utc_datetime_usec)
   end
 
   def changeset(event, attrs) do
     event
-    |> cast(attrs, [:similarity_id, :datetime, :level, :kind, :reason, :stacktrace, :log_event, :plug_conn, :metadata, :by])
+    |> cast(attrs, [
+      :similarity_id,
+      :datetime,
+      :level,
+      :kind,
+      :reason,
+      :stacktrace,
+      :log_event,
+      :plug_conn,
+      :metadata,
+      :by
+    ])
     |> validate_required([:similarity_id, :datetime, :level, :kind, :reason])
   end
 end
