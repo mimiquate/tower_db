be5aec194a81fe249579b535420666eff4e90ade
Mariana Gonzalez <mariana@mimiquate.com>
2026-07-31 14:14:16 -0300

    remove case since reason can't be changed

--- lib/tower_db/schemas/event.ex
+++ lib/tower_db/schemas/event.ex
@@ -42,16 +42,11 @@ defmodule TowerDB.Event do
   end
 
   defp put_normalized_reason(changeset) do
-    case get_change(changeset, :reason) do
-      nil ->
-        changeset
-
-      reason ->
-        kind = get_field(changeset, :kind)
-        stacktrace = get_field(changeset, :stacktrace) || []
-        normalized_reason = format_reason(kind, reason, stacktrace)
-        put_change(changeset, :normalized_reason, normalized_reason)
-    end
+    reason = get_field(changeset, :reason)
+    kind = get_field(changeset, :kind)
+    stacktrace = get_field(changeset, :stacktrace) || []
+    normalized_reason = format_reason(kind, reason, stacktrace)
+    put_change(changeset, :normalized_reason, normalized_reason)
   end
 
   # Exception.format/3 only handles :error, :exit, and :throw kinds.
