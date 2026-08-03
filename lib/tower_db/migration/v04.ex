6e3295254367397a2938e1ddcd3b665f6f510265
Mariana Gonzalez <mariana@mimiquate.com>
2026-08-03 12:14:13 -0300

    create and drop index if exists

--- lib/tower_db/migration/v04.ex
+++ lib/tower_db/migration/v04.ex
@@ -6,10 +6,10 @@ defmodule TowerDB.Migration.V04 do
   def up do
     execute("CREATE EXTENSION IF NOT EXISTS pg_trgm")
 
-    create(index(:tower_db_events, ["normalized_reason gin_trgm_ops"], using: :gin))
+    create_if_not_exists(index(:tower_db_events, ["normalized_reason gin_trgm_ops"], using: :gin))
   end
 
   def down do
-    drop(index(:tower_db_events, ["normalized_reason gin_trgm_ops"], using: :gin))
+    drop_if_exists(index(:tower_db_events, ["normalized_reason gin_trgm_ops"], using: :gin))
   end
 end
