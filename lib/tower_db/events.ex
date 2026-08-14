0d7ef5d5006e911ed7068b2346c28af58b80f87e
Mariana Gonzalez <mariana@mimiquate.com>
2026-08-14 14:16:24 -0300

    fix: remove count_distinct_similarity_ids function

--- lib/tower_db/events.ex
+++ lib/tower_db/events.ex
@@ -29,16 +29,6 @@ defmodule TowerDB.Events do
     |> repo.aggregate(:count)
   end
 
-  def count_distinct_similarity_ids(opts \\ []) do
-    repo = Keyword.get(opts, :repo) || Repo.repo()
-    filters = Keyword.get(opts, :filters, [])
-
-    Event
-    |> where(^filter_where(filters))
-    |> select([e], count(e.similarity_id, :distinct))
-    |> repo.one()
-  end
-
   defp filter_where(filters) do
     Enum.reduce(filters, dynamic(true), fn
       {:search, value}, dynamic when value != "" ->
